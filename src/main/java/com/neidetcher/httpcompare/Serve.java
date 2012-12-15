49de7ca72bcc6786cf2a57d601588e3056e4081e
Demian Neidetcher <demian0311@gmail.com>
2012-12-15 07:58:30 -0600

    add more to the test endpoint

--- src/main/java/com/neidetcher/httpcompare/Serve.java
+++ src/main/java/com/neidetcher/httpcompare/Serve.java
@@ -1,9 +1,11 @@
 package com.neidetcher.httpcompare;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -27,15 +29,29 @@ public class Serve {
     }
     
     static class TestHandler extends HandlerHelper implements HttpHandler {
-        public void handle(HttpExchange t) throws IOException {
+        public void handle(HttpExchange httpExchange) throws IOException {
         	StringBuffer sb = new StringBuffer("=== test ===");
-        	sb.append("\nprotocol      : " + t.getProtocol());
-        	sb.append("\nrequest method: " + t.getRequestMethod());
+        	sb.append("\nprotocol       : " + httpExchange.getProtocol());
+        	sb.append("\nrequest method : " + httpExchange.getRequestMethod());
+        	sb.append("\nrequest URI    : " + httpExchange.getRequestURI());
+        	sb.append("\nrequest headers: ");
         	
-        	sendString(sb.toString(), t);
+        	Headers headers = httpExchange.getRequestHeaders();
+        	for(String currKey : headers.keySet()){
+        		sb.append("\n\t" + currKey + ": " +  headers.get(currKey));
+        	}
+        	
+        	sb.append("\nrequest body   : ");
+        	sb.append(convertStreamToString(httpExchange.getRequestBody()));
+        	
+        	sendString(sb.toString(), httpExchange);
         }
     }
     
+    public static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
     
     static class TimeHandler extends HandlerHelper implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
