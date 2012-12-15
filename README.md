923d6abea5d7e010d0ab9ea75838972bfc2d773c
Demian Neidetcher <demian0311@gmail.com>
2012-12-15 00:43:00 -0600

    add a test endpoint to serve

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 This project is to play with HTTP clients on the JVM.
 
-To start up the local server
-```
-mvn exec:java
-```
-
+## To start up the local server
 <pre>
-For Ecilpse support.
+mvn package
+mvn exec:java
+</pre>
+
+## For Ecilpse support.
+<pre>
 mvn dependency:sources
 mvn dependency:resolve -Dclassifier=javadoc
 mvn eclipse:eclipse
--- src/main/java/com/neidetcher/httpcompare/Serve.java
+++ src/main/java/com/neidetcher/httpcompare/Serve.java
@@ -11,16 +11,31 @@ import com.sun.net.httpserver.HttpServer;
 /** 
  * Courtesy of
  * http://stackoverflow.com/questions/3732109/simple-http-server-in-java-using-only-java-se-api
+ * 
+ * This shows how to do HTTPS
+ * http://docs.oracle.com/javase/6/docs/jre/api/net/httpserver/spec/com/sun/net/httpserver/package-summary.html
  */
 public class Serve {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new TestHandler());
         server.createContext("/time", new TimeHandler());
         server.createContext("/ip", new IpHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
+    
+    static class TestHandler extends HandlerHelper implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+        	StringBuffer sb = new StringBuffer("=== test ===");
+        	sb.append("\nprotocol      : " + t.getProtocol());
+        	sb.append("\nrequest method: " + t.getRequestMethod());
+        	
+        	sendString(sb.toString(), t);
+        }
+    }
+    
     
     static class TimeHandler extends HandlerHelper implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
@@ -35,7 +50,6 @@ public class Serve {
     }
     static class HandlerHelper{
     	public void sendString(String response, HttpExchange t) throws IOException{
-            //String response = "ip: " + t.getRemoteAddress();
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
