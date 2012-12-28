365a2b435e1413aa3b4f633cc401c0c7a42a840b
Demian Neidetcher <demian0311@gmail.com>
2012-12-28 16:46:21 -0600

    apache weather test

---
+++ src/test/java/com/neidetcher/httpcompare/apache/WeatherTest.java
@@ -0,0 +1,81 @@
+package com.neidetcher.httpcompare.apache;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.apache.http.*;
+import org.apache.http.impl.DefaultConnectionReuseStrategy;
+import org.apache.http.impl.DefaultHttpClientConnection;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.params.*;
+import org.apache.http.protocol.*;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+/** This requires a live connection 
+ */
+public class WeatherTest {
+
+	@Test
+	public void testGet() throws UnknownHostException, IOException, HttpException {
+		HttpParams params = new SyncBasicHttpParams();
+        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setContentCharset(params, "UTF-8");
+        HttpProtocolParams.setUserAgent(params, "HttpComponents/1.1");
+        HttpProtocolParams.setUseExpectContinue(params, true);
+
+        HttpProcessor httpproc = new ImmutableHttpProcessor(new HttpRequestInterceptor[] {
+                // Required protocol interceptors
+                new RequestContent(),
+                new RequestTargetHost(),
+                // Recommended protocol interceptors
+                new RequestConnControl(),
+                new RequestUserAgent(),
+                new RequestExpectContinue()});
+
+        HttpRequestExecutor httpexecutor = new HttpRequestExecutor();
+
+        HttpContext context = new BasicHttpContext(null);
+        HttpHost host = new HttpHost("weather.yahooapis.com", 80);
+
+        DefaultHttpClientConnection conn = new DefaultHttpClientConnection();
+        ConnectionReuseStrategy connStrategy = new DefaultConnectionReuseStrategy();
+
+        context.setAttribute(ExecutionContext.HTTP_CONNECTION, conn);
+        context.setAttribute(ExecutionContext.HTTP_TARGET_HOST, host);
+
+        // go here to look up WOEIDs
+        try {
+            String[] targets = { "/forecastrss?w=2434560" };
+
+            for (int i = 0; i < targets.length; i++) {
+                if (!conn.isOpen()) {
+                    Socket socket = new Socket(host.getHostName(), host.getPort());
+                    conn.bind(socket, params);
+                }
+                BasicHttpRequest request = new BasicHttpRequest("GET", targets[i]);
+                System.out.println(">> Request URI: " + request.getRequestLine().getUri());
+
+                request.setParams(params);
+                httpexecutor.preProcess(request, httpproc, context);
+                HttpResponse response = httpexecutor.execute(request, conn, context);
+                response.setParams(params);
+                httpexecutor.postProcess(response, httpproc, context);
+
+                System.out.println("<< Response: " + response.getStatusLine());
+                System.out.println(EntityUtils.toString(response.getEntity()));
+                System.out.println("==============");
+                if (!connStrategy.keepAlive(response, context)) {
+                    conn.close();
+                } else {
+                    System.out.println("Connection kept alive...");
+                }
+            }
+        } finally {
+            conn.close();
+        }
+	
+	}
+
+}
