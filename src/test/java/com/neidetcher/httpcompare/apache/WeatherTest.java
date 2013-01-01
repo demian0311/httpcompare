342f0017ced6a0bc797df4c5e6c5974baeda4f47
Demian Neidetcher <demian0311@gmail.com>
2013-01-01 17:08:23 -0600

    clean up apache test

--- src/test/java/com/neidetcher/httpcompare/apache/WeatherTest.java
+++ src/test/java/com/neidetcher/httpcompare/apache/WeatherTest.java
@@ -1,81 +1,54 @@
 package com.neidetcher.httpcompare.apache;
 
 import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
-import org.apache.http.*;
-import org.apache.http.impl.DefaultConnectionReuseStrategy;
-import org.apache.http.impl.DefaultHttpClientConnection;
-import org.apache.http.message.BasicHttpRequest;
-import org.apache.http.params.*;
-import org.apache.http.protocol.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
-/** This requires a live connection 
+/**
+ * This requires a live connection
  */
 public class WeatherTest {
-
-	@Test
-	public void testGet() throws UnknownHostException, IOException, HttpException {
-		HttpParams params = new SyncBasicHttpParams();
-        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-        HttpProtocolParams.setContentCharset(params, "UTF-8");
-        HttpProtocolParams.setUserAgent(params, "HttpComponents/1.1");
-        HttpProtocolParams.setUseExpectContinue(params, true);
-
-        HttpProcessor httpproc = new ImmutableHttpProcessor(new HttpRequestInterceptor[] {
-                // Required protocol interceptors
-                new RequestContent(),
-                new RequestTargetHost(),
-                // Recommended protocol interceptors
-                new RequestConnControl(),
-                new RequestUserAgent(),
-                new RequestExpectContinue()});
-
-        HttpRequestExecutor httpexecutor = new HttpRequestExecutor();
-
-        HttpContext context = new BasicHttpContext(null);
-        HttpHost host = new HttpHost("weather.yahooapis.com", 80);
-
-        DefaultHttpClientConnection conn = new DefaultHttpClientConnection();
-        ConnectionReuseStrategy connStrategy = new DefaultConnectionReuseStrategy();
-
-        context.setAttribute(ExecutionContext.HTTP_CONNECTION, conn);
-        context.setAttribute(ExecutionContext.HTTP_TARGET_HOST, host);
-
-        // go here to look up WOEIDs
-        try {
-            String[] targets = { "/forecastrss?w=2434560" };
-
-            for (int i = 0; i < targets.length; i++) {
-                if (!conn.isOpen()) {
-                    Socket socket = new Socket(host.getHostName(), host.getPort());
-                    conn.bind(socket, params);
-                }
-                BasicHttpRequest request = new BasicHttpRequest("GET", targets[i]);
-                System.out.println(">> Request URI: " + request.getRequestLine().getUri());
-
-                request.setParams(params);
-                httpexecutor.preProcess(request, httpproc, context);
-                HttpResponse response = httpexecutor.execute(request, conn, context);
-                response.setParams(params);
-                httpexecutor.postProcess(response, httpproc, context);
-
-                System.out.println("<< Response: " + response.getStatusLine());
-                System.out.println(EntityUtils.toString(response.getEntity()));
-                System.out.println("==============");
-                if (!connStrategy.keepAlive(response, context)) {
-                    conn.close();
-                } else {
-                    System.out.println("Connection kept alive...");
-                }
-            }
-        } finally {
-            conn.close();
-        }
 	
-	}
+	private HttpClient createClient(int poolSize, int timeout) {
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(
+		         new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
 
+		PoolingClientConnectionManager cm = new PoolingClientConnectionManager(schemeRegistry);
+		// https://hc.apache.org/httpcomponents-client-ga/httpclient/apidocs/org/apache/http/impl/conn/PoolingClientConnectionManager.html
+		cm.setDefaultMaxPerRoute(poolSize);
+		 
+		HttpClient httpClient = new DefaultHttpClient(cm);
+		// https://hc.apache.org/httpcomponents-core-ga/httpcore/apidocs/org/apache/http/params/CoreConnectionPNames.html
+		
+		// Determines the timeout in milliseconds until a connection is 
+		// established.
+		httpClient.getParams().setParameter(HttpConnectionParams.CONNECTION_TIMEOUT, timeout);
+		
+		// Defines the socket timeout (SO_TIMEOUT) in milliseconds, which is 
+		// the timeout for waiting for data or, put differently, a maximum 
+		// period inactivity between two consecutive data packets).
+		httpClient.getParams().setParameter(HttpConnectionParams.SO_TIMEOUT, timeout);
+		
+		return httpClient;
+	}
+	
+	@Test public void testFoo() throws Throwable, IOException {
+		HttpClient client = createClient(200, 1000);
+		HttpUriRequest request = new HttpGet("http://weather.yahooapis.com/forecastrss?w=12788310");
+		HttpResponse response = client.execute(request);
+		System.out.println("response: " + response.getStatusLine());
+		System.out.println(EntityUtils.toString(response.getEntity()));
+	}
 }
