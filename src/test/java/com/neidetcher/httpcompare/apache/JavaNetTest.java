4f31b67924f0a08b2471001a122d8d047824ee1d
Demian Neidetcher <demian0311@gmail.com>
2013-01-05 14:54:58 -0600

    add javanet test

---
+++ src/test/java/com/neidetcher/httpcompare/apache/JavaNetTest.java
@@ -0,0 +1,38 @@
+package com.neidetcher.httpcompare.apache;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Test;
+
+/**
+ * This requires a live connection
+ */
+public class JavaNetTest {
+	
+	private HttpURLConnection createClient(String url, int poolSize, int timeout) throws Throwable {
+		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+		con.setConnectTimeout(timeout);
+	
+		return con;
+	}
+	
+	@Test public void testFoo() throws Throwable, IOException {
+		
+		HttpURLConnection client = createClient(
+				"http://weather.yahooapis.com/forecastrss?w=12788310", 
+				200, 1000);
+		int responseCode = client.getResponseCode();
+		System.out.println("response: " + responseCode);
+		InputStream content = (InputStream)client.getContent();
+	    Reader r = new InputStreamReader(content);
+	    int c;
+	    while ((c = r.read()) != -1) {
+	      System.out.print((char) c);
+	    }
+	}
+}
